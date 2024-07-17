0514c282da4d19522de21c6bf06c355c712737f1
Odafe Aror <odafe41@gmail.com>
2024-07-17 02:43:11 +0000

    wip: new sessionSigs function

--- src/app/_components/lit-encrypt.tsx
+++ src/app/_components/lit-encrypt.tsx
@@ -24,7 +24,6 @@ export default function LitEncryption() {
   const [file, setFile] = useState(null);
   const [encryptedFile, setEncryptedFile] = useState<any>(null);
   const [litNodeClient, setLitNodeClient] = useState<LitJsSdk.LitNodeClient>();
-  const [sessionSigs, setSessionSigs] = useState<SessionSigsMap>();
   const { address } = useAccount();
   const { signMessage } = useSignMessage();
 
@@ -32,73 +31,98 @@ export default function LitEncryption() {
     const initLitClient = async () => {
       try {
         const client = new LitJsSdk.LitNodeClient({
-          litNetwork: LitNetwork.DatilDev,
+          litNetwork: LitNetwork.Cayenne,
         });
         await client.connect();
         setLitNodeClient(client);
+        console.log("connected", litNodeClient);
       } catch (error) {
-        console.error("error setting session sigs", error);
+        console.error("error connecting client", error);
       }
     };
 
     initLitClient();
   }, []);
 
-  useEffect(() => {
-    if (litNodeClient) {
-      const initSessionSigs = async () => {
-        try {
-          const _sessionSigs = await litNodeClient.getSessionSigs({
-            chain,
-            resourceAbilityRequests: [
-              {
-                resource: new LitAccessControlConditionResource("someResource"),
-                ability: LitAbility.AccessControlConditionDecryption,
-              },
-            ],
-            switchChain: true,
-            authNeededCallback,
-          });
-          setSessionSigs(_sessionSigs);
-        } catch (error) {
-          console.error("error setting session sigs", error);
-        }
-      };
+  // useEffect(() => {
+  //   if (litNodeClient) {
+  //     const initSessionSigs = async () => {
+  //       try {
+  //         const _sessionSigs = await litNodeClient.getSessionSigs({
+  //           chain,
+  //           resourceAbilityRequests: [
+  //             {
+  //               resource: new LitAccessControlConditionResource("*"),
+  //               ability: LitAbility.AccessControlConditionDecryption,
+  //             },
+  //           ],
+  //           switchChain: true,
+  //           authNeededCallback,
+  //         });
+  //         setSessionSigs(_sessionSigs);
+  //       } catch (error) {
+  //         console.error("error setting session sigs", error);
+  //       }
+  //     };
 
-      initSessionSigs();
-    }
-  }, [litNodeClient]);
+  //     initSessionSigs();
+  //   }
+  // }, [litNodeClient]);
 
   const handleFileChange = (e: any) => {
     setFile(e.target.files[0]);
   };
 
-  const authNeededCallback = async ({
-    uri,
-    expiration,
-    resourceAbilityRequests,
-  }: AuthCallbackParams) => {
-    // Prepare the SIWE message for signing
-    const toSign = await createSiweMessageWithRecaps({
-      uri: uri!,
-      expiration: expiration!,
-      resources: resourceAbilityRequests!,
-      walletAddress: address as string,
-      nonce: await litNodeClient!.getLatestBlockhash(),
-      litNodeClient: litNodeClient,
-    });
-    // Use the Ethereum wallet to sign the message, return the digital signature
-    const signature = await signMessage({ message: toSign });
+  const getSessionSignatures = async () => {
+    if (!file || !litNodeClient) return;
+    // Get the latest blockhash
+    const latestBlockhash = await litNodeClient.getLatestBlockhash();
 
-    // Create an AuthSig using the derived signature, the message, and wallet address
-    const authSig: AuthSig = {
-      sig: signature,
-      derivedVia: "web3.eth.personal.sign",
-      signedMessage: toSign,
-      address: address as string,
+    // Define the authNeededCallback function
+    const authNeededCallback = async ({
+      uri,
+      expiration,
+      resourceAbilityRequests,
+    }: AuthCallbackParams) => {
+      // Prepare the SIWE message for signing
+      const toSign = await createSiweMessageWithRecaps({
+        uri: uri!,
+        expiration: expiration!,
+        resources: resourceAbilityRequests!,
+        walletAddress: address as string,
+        nonce: latestBlockhash,
+        litNodeClient: litNodeClient,
+      });
+      // Use the Ethereum wallet to sign the message, return the digital signature
+      const signature = await signMessage({ message: toSign });
+
+      // Create an AuthSig using the derived signature, the message, and wallet address
+      const authSig: AuthSig = {
+        sig: signature,
+        derivedVia: "web3.eth.personal.sign",
+        signedMessage: toSign,
+        address: address as string,
+      };
+
+      return authSig;
     };
 
-    return authSig;
+    // Define the Lit resource
+    const litResource = new LitAccessControlConditionResource("*");
+
+    // Get the session signatures
+    const sessionSigs = await litNodeClient.getSessionSigs({
+      chain: chain,
+      resourceAbilityRequests: [
+        {
+          resource: litResource,
+          ability: LitAbility.AccessControlConditionDecryption,
+        },
+      ],
+      authNeededCallback,
+      // capacityDelegationAuthSig,
+    });
+    return sessionSigs;
   };
 
   const accessControlConditions = [
@@ -117,9 +141,16 @@ export default function LitEncryption() {
   const encryptFile = async () => {
     if (!file || !litNodeClient) return;
 
+    
     try {
+      const sessionSigs = await getSessionSignatures();
       const { ciphertext, dataToEncryptHash } = await LitJsSdk.encryptFile(
-        { file, chain, accessControlConditions, sessionSigs },
+        {
+          file,
+          chain,
+          accessControlConditions,
+          // sessionSigs
+        },
         litNodeClient
       );
 
@@ -133,13 +164,18 @@ export default function LitEncryption() {
     } catch (error) {
       console.error("error encrypting", error);
     }
-    litNodeClient.disconnect();
+    await litNodeClient.disconnect();
   };
 
   const decryptFile = async () => {
     if (!encryptedFile || !litNodeClient) return;
 
+    
     try {
+      const sessionSigs = await getSessionSignatures();
+  
+      console.log("sessionsigs", sessionSigs);
+      console.log("encyrptmeta", encryptedFile);
       const decryptedFileUint8 = await LitJsSdk.decryptToFile(
         {
           ciphertext: encryptedFile.ciphertext,
@@ -150,6 +186,8 @@ export default function LitEncryption() {
         },
         litNodeClient
       );
+
+      console.log("decrypted file", decryptedFileUint8);
 
       const decryptedFile = new Blob([
         uint8arrayToString(decryptedFileUint8, "utf8"),
@@ -164,7 +202,11 @@ export default function LitEncryption() {
       console.error("error decrypting", error);
     }
 
-    litNodeClient.disconnect();
+    await litNodeClient.disconnect();
+  };
+
+  const disconnect = () => {
+    LitJsSdk.disconnectWeb3();
   };
 
   return (
@@ -172,6 +214,9 @@ export default function LitEncryption() {
       <input type="file" onChange={handleFileChange} />
       <button onClick={encryptFile}>Encrypt File</button>
       <button onClick={decryptFile}>Decrypt File</button>
+      <div>
+        <button onClick={disconnect}>disconnect</button>
+      </div>
     </div>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
+"use client";
 import { ConnectButton } from "@rainbow-me/rainbowkit";
 import LitEncryption from "./_components/lit-encrypt";
+import { useAccount } from "wagmi";
 
 export default function Home() {
+  const { address, isConnected } = useAccount();
+
   return (
     <div>
       <ConnectButton />
-      <LitEncryption />
+      {isConnected && <LitEncryption />}
     </div>
   );
 }
